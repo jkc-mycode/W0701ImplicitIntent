a4a459c0d066d2330daac0a215fef5a8924a0604
jkc-mycode <jkc9709@naver.com>
2022-10-19 14:32:42 +0900

    :white_check_mark: Update Source

--- app/src/main/java/kr/ac/kumoh/s20201414/w0701implicitintent/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20201414/w0701implicitintent/MainActivity.kt
@@ -19,6 +19,18 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(Intent.ACTION_VIEW, uri)
             startActivity(intent)
         }
+        binding.btnMap.setOnClickListener{
+            //val uri = Uri.parse("geo:36.145014,128.393047?z=17")
+            val uri = Uri.parse("geo:0,0?z=17&q=동대구역")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+        binding.btnTelephone.setOnClickListener{
+            //val uri = Uri.parse("geo:36.145014,128.393047?z=17")
+            val uri = Uri.parse("sms:010-6238-4253")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
 
     }
 }
